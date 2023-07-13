58dde84ac2f8bdb2511777db236892226ac6da37
Imanuel Pundoko <ilomon10@gmail.com>
2023-07-13 15:28:20 +0800

    Add start script for common js env

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const host = app.get('host')
 
 process.on('unhandledRejection', (reason) => logger.error('Unhandled Rejection %O', reason))
 
+console.log(app.get("mysql"))
+
 app.listen(port).then(() => {
   logger.info(`Feathers app listening on http://${host}:${port}`)
 })
